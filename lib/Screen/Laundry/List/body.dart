0ba0bf14aca06842f77c58d95688ebb696279631
Akmal Kamarolzaman <akmalkmrlzmn@gmail.com>
2021-06-18 13:44:28 +0800

    laundry-list commit

---
+++ lib/Screen/Laundry/List/body.dart
@@ -0,0 +1,52 @@
+//import 'package:ajaib_laundary/Screen/Laundry/list/list.dart';
+
+import 'list.dart';
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  const Body({state}) : _state = state;
+
+  final ListScreenState _state;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Title',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text('subtitle'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Title',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text('subtitle'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
