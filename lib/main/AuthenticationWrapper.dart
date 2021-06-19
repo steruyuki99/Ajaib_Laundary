1a6e242f3ae9465564dbd4ce9cce41e9a1c0137e
asyahir99 <ahmadsyahir023@gmail.com>
2021-06-19 14:47:02 +0800

    new firebase

--- lib/main/AuthenticationWrapper.dart
+++ lib/main/AuthenticationWrapper.dart
@@ -1,6 +1,5 @@
 import 'package:ajaib_laundary/Screen/Laundry/List/list.dart';
 import 'package:ajaib_laundary/Screen/Login/login.dart';
-import 'package:ajaib_laundary/Screen/register/register.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
